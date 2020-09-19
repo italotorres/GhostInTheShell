2fa7d01debfec4dfe67af402a919dd852c86fd0b
italotorres <italotorres3@gmail.com>
2020-09-19 15:25:26 -0300

    Efetuando melhorias no script

--- gerencia_grupo.sh
+++ gerencia_grupo.sh
@@ -7,7 +7,7 @@
 #
 # Autor Ítalo Torres(italotorres3@gmail.com)
 # Data de criação: 16/09/2020
-#
+# Data de atualização 19/09/2020
 # Descrição: script de exemplo para facilitar
 # a criação de usuarios e grupos.
 #
@@ -24,8 +24,10 @@ echo "Escript para Gerenciamento de contas de usuarios e grupos deste sistema"
 sleep 3
 echo "Por favor preencha os dados na forma da solicitação"
 echo ""
-read -p "Informe o nome do grupo a ser cirado: " GRUPO
+
+read -p "Informe o nome do grupo a ser criado: " GRUPO
 COMPARA_GRU=$(cat /etc/group | cut -d: -f1 | grep $GRUPO )
+
 if [ "$GRUPO" = "$COMPARA_GRU" ];
 then
 	echo "Ja existe este grupo no sistema, por favor informe um nome diferente para o grupo"
@@ -34,13 +36,16 @@ then
 else
 	echo "Criando um grupo..."
 	sleep 3
-	sudo groupadd $GRUPO > /dev/null 2>&1
+	groupadd $GRUPO > /dev/null 2>&1
 
 fi
 
 clear
+
 read -p "Informe o nome do usuário a ser cirado: " USUARIO
 COMPARA_USU=$(cat /etc/passwd | cut -d: -f1  | grep $USUARIO)
+
+
 
 if [ "$USUARIO" = "$COMPARA_USU" ];
 then
@@ -51,9 +56,9 @@ else
 
 	echo "Efetuando cadastro do usuário..."
 	sleep 3
-	sudo useradd -g $GRUPO -NM -s /bin/bash $USUARIO > /dev/null 2>&1
+	 useradd -g $GRUPO -NM -s /bin/bash $USUARIO > /dev/null 2>&1
 	sleep 3
-	sudo passwd $USUARIO 
+	 passwd $USUARIO 
 	echo "Usuário e senha criada com sucesso"
 	sleep 3
 
@@ -63,13 +68,14 @@ clear
 
 echo "O usuário foi cadastrado nos sequintes formato [Usuário:GRUPO]: "
 echo ""
-echo "$(sudo groups $USUARIO)"
-sudo mkdir /$GRUPO
+echo "$(groups $USUARIO)"
+ mkdir /$GRUPO
 echo ""
-sudo chmod 1770 /$GRUPO
-sudo chown $USUARIO:$GRUPO /$GRUPO
+ chmod 1770 /$GRUPO
+ chown $USUARIO:$GRUPO /$GRUPO
 echo "O diretorio com as devidas permissões foram criadas conforme pode ser visto abaixo:"
 echo ""
-echo "$(sudo ls -la / | grep /$GRUPO )"
-
+ls -lda /$GRUPO
+echo " "
 echo "Fim da Execução!"
+sleep 3
